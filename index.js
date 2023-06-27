314e949de96b67a63758a2d600cd4be32886e9ee
Ramazan <r.tokoshev@gmail.com>
2023-06-27 19:41:01 +0600

    feat: created validation for the user, checked sending to create the user, tied the logic to create the token

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {validationResult} from "express-validator";
+import {registerValidation} from './validations/auth.js';
+import UserModel from './models/Users.js';
 
-mongoose.connect('mongodb+srv://admin:admin123@cluster0.enhkmrq.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.enhkmrq.mongodb.net/blog?retryWrites=true&w=majority')
 		.then(() => console.log('DB OK'))
 		.catch((err) => console.log('DB ERROR', err));
 
@@ -15,11 +19,44 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-// app.post('/auth/login', (req, res) => {
-// });
+app.post('/auth/register', registerValidation, async (req, res) => {
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json(errors.array());
+		}
 
-app.post('/auth/register', (req, res) => {
+		const password = req.body.password;
+		const salt = await bcrypt.genSalt(5);
+		const hash = await bcrypt.hash(password, salt);
 
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash: hash,
+		});
+
+		const user = await doc.save();
+
+		const token = jwt.sign({
+			_id: user._id,
+		}, 'secret123', {expiresIn: '30d',});
+
+		const {passwordHash, ...userData} = user._doc;
+
+		res.json({
+			...userData,
+			token
+		});
+
+
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: 'Не удалось зарегестрироваться'
+		});
+	}
 });
 
 app.listen(PORT, (err) => {
@@ -29,4 +66,3 @@ app.listen(PORT, (err) => {
 
 	console.log(`Server started on ${PORT} port!`);
 });
-
