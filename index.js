e73bb337646d15a7215fcb69ebdf001d50da3f9c
Ramazan <r.tokoshev@gmail.com>
2023-06-30 17:05:37 +0600

    feat: create utils chechAuth, create controllers Posts, Users, create Model Post

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import {validationResult} from "express-validator";
-import {registerValidation} from './validations/auth.js';
-import UserModel from './models/Users.js';
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
+import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose.connect('mongodb+srv://admin:admin123@cluster0.enhkmrq.mongodb.net/blog?retryWrites=true&w=majority')
 		.then(() => console.log('DB OK'))
@@ -15,49 +15,15 @@ const PORT = 4444;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
+app.post('/auth/login', loginValidation, UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-	try {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array());
-		}
-
-		const password = req.body.password;
-		const salt = await bcrypt.genSalt(5);
-		const hash = await bcrypt.hash(password, salt);
-
-		const doc = new UserModel({
-			email: req.body.email,
-			fullName: req.body.fullName,
-			avatarUrl: req.body.avatarUrl,
-			passwordHash: hash,
-		});
-
-		const user = await doc.save();
-
-		const token = jwt.sign({
-			_id: user._id,
-		}, 'secret123', {expiresIn: '30d',});
-
-		const {passwordHash, ...userData} = user._doc;
-
-		res.json({
-			...userData,
-			token
-		});
-
-
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({
-			message: 'Не удалось зарегестрироваться'
-		});
-	}
-});
+// app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+// app.delete('/posts', checkAuth, PostController.remove);
+// app.patch('/posts', checkAuth, PostController.update);
 
 app.listen(PORT, (err) => {
 	if (err) {
