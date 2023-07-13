aac897e0b6c59b2bf9ca86d7f6008dee29d12b45
Ramazan <r.tokoshev@gmail.com>
2023-07-13 19:27:07 +0600

    feat: add cors and new Controller Post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {config} from 'dotenv';
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
 import {UserController , PostController} from './controllers/index.js';
@@ -26,6 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
@@ -39,6 +41,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 });
 
 app.get('/posts', PostController.getAll);
+app.get('/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
